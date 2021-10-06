ea326470b587f608107460706c2a89d8525de0fb
Nikhil Cherukupalli <nikhilrcherukupalli@gmail.com>
2021-10-06 11:00:26 -0400

    added docstrings, optimized imports

--- lib/visualizations.py
+++ lib/visualizations.py
@@ -3,37 +3,51 @@ Filename: visualizations.py
 Author: Nikhil Cherukupalli
 """
 
-from wordcloud import WordCloud
 from matplotlib import pyplot as plt
+from wordcloud import WordCloud
 
 
 def display_wordcloud(data, title=None, save_fig_path=None):
-    cloud = WordCloud(
-        background_color='white',
-        max_words=200,
-        max_font_size=40,
-        scale=3,
-        random_state=42
-    ).generate(str(data))
-
+    """ Displays wordcloud highlighting words appearing frequently.
+    :param data (list-like): Contains strings of words/sentences.
+    :param title (str): Title of wordcloud
+    :param save_fig_path (str): Path to save file
+    """
+    # Build figure
+    cloud = WordCloud(background_color='white',
+                      max_words=200,
+                      max_font_size=40,
+                      scale=3,
+                      random_state=42
+                      ).generate(str(data))
     fig = plt.figure(1, figsize=(10, 10))
     plt.axis('off')
     if title:
         fig.suptitle(title, fontsize=20)
         fig.subplots_adjust(top=2.3)
-
     plt.imshow(cloud)
+
     plt.show()
     if save_fig_path:
         fig.savefig(save_fig_path, bbox_inches="tight")
 
 
 def display_elbow_chart(lst_inertia, save_fig_path=None):
-    fig, axs = plt.figure(figsize=(16, 9))
+    """ Displays line chart of model inertias. Useful for KMeans to find
+        the optimal k.
+    :param lst_inertia ([float]): Model inertia results
+    :param save_fig_path (str): Path to save file
+    :return:
+    """
+    # Build figure
+    fig = plt.figure(1, figsize=(10, 10))
+    plt.plot(list(range(1, len(lst_inertia) + 1)),
+             lst_inertia,
+             "kx-")
 
-    axs.plot(list(range(1, len(lst_inertia) + 1)), lst_inertia, "kx-")
-    axs.set_xlabel("# of Clusters")
-    axs.set_ylabel("Inertia")
+    plt.xlabel("# of Clusters")
+    plt.ylabel("Inertia")
+    plt.grid()
 
     plt.show()
     if save_fig_path:
